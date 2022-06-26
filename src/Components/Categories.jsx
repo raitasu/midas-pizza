6cd47e9a538701c88f4560de4d07beacd40df9c7
raitasu <pavelburyak3@gmail.com>
2022-06-26 17:13:59 +0300

    feat: added useState for categories and pizzaItem

--- src/Components/Categories.jsx
+++ src/Components/Categories.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
 const Categories = () => {
+
+    const [activeIndex, setActiveIndex] = React.useState(0)
+
+
+    const changeIndexValue = (index) => {
+        setActiveIndex(index)
+    }
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                <li>Мясные</li>
-                <li>Вегетарианская</li>
-                <li>Гриль</li>
-                <li>Острые</li>
-                <li>Закрытые</li>
+                <li className={activeIndex === 0 ? 'active' : ''} onClick={() => changeIndexValue(0)}>Все
+                </li>
+                <li className={activeIndex === 1 ? 'active' : ''} onClick={() => changeIndexValue(1)}>Мясные
+                </li>
+                <li className={activeIndex === 2 ? 'active' : ''} onClick={() => changeIndexValue(2)}>Вегетарианская
+                </li>
+                <li className={activeIndex === 3 ? 'active' : ''} onClick={() => changeIndexValue(3)}>Гриль
+                </li>
+                <li className={activeIndex === 4 ? 'active' : ''} onClick={() => changeIndexValue(4)}>Острые
+                </li>
+                <li className={activeIndex === 5 ? 'active' : ''} onClick={() => changeIndexValue(5)}>Закрытые
+                </li>
             </ul>
         </div>
 
